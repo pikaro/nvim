5d6a8290ee52faef14cac7a454998162f0c17d6e
David Reis <post@d-reis.com>
2025-03-27 15:26:44 +0100

    Add more treesitter object mappings

--- lua/plugins/config/treesitter_textobjects.lua
+++ lua/plugins/config/treesitter_textobjects.lua
@@ -4,29 +4,74 @@ return function()
 			select = {
 				enable = true,
 				keymaps = {
-					["af"] = "@function.outer",
 					["if"] = "@function.inner",
-					["aF"] = "@function.outer",
-					["iF"] = "@function.inner",
-					["ac"] = "@comment.outer",
 					["ic"] = "@comment.inner",
-					["al"] = "@loop.outer",
 					["il"] = "@loop.inner",
-					["ab"] = "@block.outer",
 					["ib"] = "@block.inner",
-					["aa"] = "@parameter.outer",
 					["ia"] = "@parameter.inner",
 					["ii"] = "@conditional.inner",
-					["ai"] = "@conditional.outer",
+					["is"] = "@scope.local",
+					["of"] = "@function.outer",
+					["oc"] = "@comment.outer",
+					["ol"] = "@loop.outer",
+					["ob"] = "@block.outer",
+					["oa"] = "@parameter.outer",
+					["oi"] = "@conditional.outer",
+					["os"] = "@scope.outer",
 				},
 			},
-			lsp_interop = {
+			move = {
 				enable = true,
-				peek_definition_code = {
-					["<leader>af"] = "@function.outer",
-					["<leader>aF"] = "@class.outer",
+				set_jumps = false, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]f"] = "@function.outer",
+					["]l"] = "@loop.outer", -- { query = { "@loop.inner", "@loop.outer" } },
+					["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+					["]b"] = "@block.outer",
+					["]c"] = "@conditional.outer",
+					["]p"] = "@parameter.outer",
 				},
+				goto_next_end = {
+					["]F"] = "@function.outer",
+					["]L"] = "@loop.outer",
+					["]S"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+					["]B"] = "@block.outer",
+					["]C"] = "@conditional.outer",
+					["]P"] = "@parameter.outer",
+				},
+				goto_previous_start = {
+					["[f"] = "@function.outer",
+					["[l"] = "@loop.outer",
+					["[s"] = { query = "@local.scope", query_group = "locals", desc = "Previous scope" },
+					["[b"] = "@block.outer",
+					["[c"] = "@conditional.outer",
+					["[p"] = "@parameter.outer",
+				},
+				goto_previous_end = {
+					["[F"] = "@function.outer",
+					["[L"] = "@loop.outer",
+					["[S"] = { query = "@local.scope", query_group = "locals", desc = "Previous scope" },
+					["[B"] = "@block.outer",
+					["[C"] = "@conditional.outer",
+					["[P"] = "@parameter.outer",
+				},
+				-- Below will go to either the start or the end, whichever is closer.
+				-- Use if you want more granular movements
+				-- Make it even more gradual by adding multiple queries and regex.
+				-- goto_next = {
+				-- 	["]d"] = "@conditional.outer",
+				-- },
+				-- goto_previous = {
+				-- 	["[d"] = "@conditional.outer",
+				-- },
 			},
+			-- lsp_interop = {
+			-- 	enable = true,
+			-- 	peek_definition_code = {
+			-- 		["<leader>af"] = "@function.outer",
+			-- 		["<leader>aF"] = "@class.outer",
+			-- 	},
+			-- },
 		},
 	})
 end

b7ecd84fbf61fe27ec60e2ae3a0d4004da341ea6
David Reis <post@d-reis.com>
2025-03-27 15:17:25 +0100

    fix: Go LSP setup -> efm

--- lua/lsp/efm.lua
+++ lua/lsp/efm.lua
@@ -3,7 +3,7 @@ local lspconfig = require("lspconfig")
 return {
 	lsp = lspconfig.efm,
 	init_options = { documentFormatting = true },
-	filetypes = { "lua", "dockerfile", "markdown" },
+	filetypes = { "lua", "dockerfile", "markdown", "go" },
 	settings = {
 		rootMarkers = { ".git/" },
 		languages = {
@@ -12,6 +12,17 @@ return {
 			markdown = {
 				require("efmls-configs.linters.markdownlint"),
 				require("efmls-configs.formatters.mdformat"),
+			},
+			go = {
+				{
+					formatCommand = string.format(
+						-- TODO: This issue says gofumpt . needs to be used, but that causes nonzero return
+						-- https://github.com/segmentio/golines/issues/100
+						"%s --base-formatter=gofumpt --no-reformat-tags",
+						require("efmls-configs.fs").executable("golines")
+					),
+					formatStdin = true,
+				},
 			},
 		},
 	},
--- lua/lsp/go.lua
+++ lua/lsp/go.lua
@@ -1,18 +1,53 @@
 local lspconfig = require("lspconfig")
 
+local downgrade = {
+	["UnusedImport"] = vim.diagnostic.severity.WARN,
+	["UnusedVar"] = vim.diagnostic.severity.WARN,
+	["UnusedParam"] = vim.diagnostic.severity.WARN,
+}
+
+local filter_handler = function(err, result, ctx, config)
+	if not result then
+		return
+	end
+	local diagnostics = result.diagnostics
+	if not diagnostics then
+		return
+	end
+	for _, diagnostic in ipairs(diagnostics) do
+		if diagnostic.source == "compiler" then
+			local severity = downgrade[diagnostic.code]
+			if severity then
+				diagnostic.severity = severity
+			end
+		end
+	end
+	vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx, config)
+end
+
 return {
 	lsp = lspconfig.gopls,
 	filetypes = { "go", "gomod", "gowork", "gotmpl" },
 	settings = {
 		gopls = {
-			gofumpt = true,
+			-- See efm.lua - wrapped by golines
+			gofumpt = false,
 			analyses = {
+				-- All other analyzers are enabled by default
 				unusedparams = true,
 				unusedvariable = true,
+				unusedwrite = true,
 				shadow = true,
 				useany = true,
 			},
 			staticcheck = true,
 		},
+	},
+	diagnostics = {
+		severity_sort = true,
+		underline = true,
+	},
+	handlers = {
+		["textDocument/publishDiagnostics"] = vim.lsp.with(filter_handler, {}),
 	},
 }

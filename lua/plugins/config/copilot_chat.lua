75af85058cfb4fd51ba93766c60ef1cd0a266eed
David Reis <post@d-reis.com>
2025-03-27 15:24:25 +0100

    Disable chat clearing

--- lua/plugins/config/copilot_chat.lua
+++ lua/plugins/config/copilot_chat.lua
@@ -1,7 +1,7 @@
 return function()
 	require("CopilotChat").setup({
 		layout = "vertical",
-		clear_chat_on_new_prompt = true,
+		clear_chat_on_new_prompt = false,
 		mappings = {
 			-- Conflicts with Kitty navigation
 			-- Not possible to set to nil

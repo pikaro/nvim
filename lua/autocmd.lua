d0b9cf9cf19eaa71c9e2cd3dd62f681e5a1dc726
David Reis <post@d-reis.com>
2025-03-27 15:22:00 +0100

    fix: Line number display

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -8,18 +8,17 @@ local function aucmd(event, pattern, callback)
 	})
 end
 
-aucmd({ "BufEnter", "FocusGained", "InsertLeave" }, "*", function()
-	local buf = vim.api.nvim_get_current_buf()
-	local listed = vim.api.nvim_buf_get_option(buf, "buflisted")
-	local modifiable = vim.api.nvim_buf_get_option(buf, "modifiable")
+aucmd({ "BufEnter", "FocusGained", "InsertLeave" }, "*", function(event)
+	local listed = vim.api.nvim_get_option_value("buflisted", { buf = event.buf })
+	local modifiable = vim.api.nvim_get_option_value("modifiable", { buf = event.buf })
 	if not listed or not modifiable then
 		return
 	end
-	vim.o.relativenumber = true
+	vim.wo.relativenumber = true
 end)
 
 aucmd({ "BufLeave", "FocusLost", "InsertEnter" }, "*", function()
-	vim.o.relativenumber = false
+	vim.wo.relativenumber = false
 end)
 
 if vim.fn.has("gui_running") then
@@ -38,3 +37,7 @@ aucmd({ "BufWritePost" }, "*.puml", function()
 	local command = string.format("cat %s %s | plantuml -darkmode -tpng -pipe > %s &", configpath, filepath, outputpath)
 	vim.fn.system(command)
 end)
+
+-- aucmd({ "BufWritePost" }, "*.go", function()
+-- 	vim.cmd("silent! GoCoverage")
+-- end)

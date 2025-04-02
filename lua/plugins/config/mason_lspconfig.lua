ccc2c1cad03fbcdd7a3e33368f19594af8ed4129
David Reis <git@d-reis.com>
2025-04-02 19:44:42 +0200

    Add nil_ls

--- lua/plugins/config/mason_lspconfig.lua
+++ lua/plugins/config/mason_lspconfig.lua
@@ -1,30 +1,31 @@
 return function()
-	require("mason-lspconfig").setup({
-		ensure_installed = {
-			"ansiblels",
-			"bashls",
-			-- TODO: How to re-add clang-tidy / clang-format config files?
-			"clangd",
-			"cssls",
-			"docker_compose_language_service",
-			"dockerls",
-			"efm",
-			-- TODO: How to correctly resolve includes in CI repo?
-			"gitlab_ci_ls",
-			"gopls",
-			"helm_ls",
-			-- FIXME: Does not seem to be doing anything
-			"jinja_lsp",
-			"jsonls",
-			"lua_ls",
-			"phpactor",
-			"pyright",
-			"rust_analyzer",
-			"terraformls",
-			"texlab",
-			"ts_ls",
-			"yamlls",
-		},
-		automatic_installation = true,
-	})
+    require("mason-lspconfig").setup({
+        ensure_installed = {
+            "ansiblels",
+            "bashls",
+            -- TODO: How to re-add clang-tidy / clang-format config files?
+            "clangd",
+            "cssls",
+            "docker_compose_language_service",
+            "dockerls",
+            "efm",
+            -- TODO: How to correctly resolve includes in CI repo?
+            "gitlab_ci_ls",
+            "gopls",
+            "helm_ls",
+            -- FIXME: Does not seem to be doing anything
+            "jinja_lsp",
+            "jsonls",
+            "lua_ls",
+            "nil_ls",
+            "phpactor",
+            "pyright",
+            "rust_analyzer",
+            "terraformls",
+            "texlab",
+            "ts_ls",
+            "yamlls",
+        },
+        automatic_installation = true,
+    })
 end

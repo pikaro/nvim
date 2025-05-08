a01ac0ca556ded2b37e5c64f055e1a42c4c4792b
David Reis <git@d-reis.com>
2025-05-08 10:32:59 +0200

    Add more Copilot bindings

--- lua/plugins/config/copilot.lua
+++ lua/plugins/config/copilot.lua
@@ -5,8 +5,13 @@ return function()
 			keymap = {
 				accept = "<a-cr>",
 				accept_line = "<a-s-cr>",
+				accept_word = "<a-s-l>",
+				next = "<a-s-j>",
+				prev = "<a-s-k>",
+				dismiss = "<a-s-h>",
 			},
 		},
+		copilot_model = "gpt-4o-copilot",
 		filetypes = {
 			yaml = true,
 			json = false,

8760210d674a8ea0ece1a66a96f409df9bfe04a8
David Reis <git@d-reis.com>
2025-06-09 20:22:07 +0200

    feat: Use custom-built clangd

--- lua/lsp/c.lua
+++ lua/lsp/c.lua
@@ -4,7 +4,15 @@ return {
 	lsp = lspconfig.clangd,
 
 	cmd = {
-		"clangd",
+		-- WARNING: Need custom-compiled clangd
+		--          git clone https://github.com/espressif/llvm-project.git
+		--          cd llvm-project
+		--          cmake -S llvm -B build -G Ninja -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"
+		--          cmake --build build
+		"/Users/david.reis/src/misc/llvm-project/build/bin/clangd",
+		-- WARNING: Need platformio toolchain
+		--          pio project init --board arduino_nano_esp32 --ide vim
+		"--query-driver=/Users/david.reis/.platformio/packages/toolchain-xtensa-esp32s3/bin/xtensa-esp32s3-elf-gcc*,/Users/david.reis/.platformio/packages/toolchain-xtensa-esp32s3/bin/xtensa-esp32s3-elf-g++*,xtensa-esp32s3-elf-gcc*,xtensa-esp32s3-elf-g++*",
 		"--background-index",
 		"--suggest-missing-includes",
 		"--clang-tidy",

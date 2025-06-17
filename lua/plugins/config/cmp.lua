2959b4140c6af828a6c032af64510df3332e4c1e
David Reis <git@d-reis.com>
2025-06-17 12:49:52 +0200

    fix: Dunder method sorting

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -18,6 +18,18 @@ return function()
 				show_labelDetails = true,
 			}),
 		},
+		sorting = {
+			comparators = {
+				cmp.config.compare.offset,
+				cmp.config.compare.exact,
+				cmp.config.compare.score,
+				require("cmp-under-comparator").under,
+				cmp.config.compare.kind,
+				cmp.config.compare.sort_text,
+				cmp.config.compare.length,
+				cmp.config.compare.order,
+			},
+		},
 		-- snippet = {
 		-- 	expand = function(args)
 		-- 		require("luasnip").lsp_expand(args.body)

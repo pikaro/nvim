9d4299a3a417f24cd4e16ed611cb5ed4c2f8092b
David Reis <git@d-reis.com>
2025-08-08 20:31:17 +0200

    feat: Dynamically determine clangd

--- lua/lsp/c.lua
+++ lua/lsp/c.lua
@@ -3,6 +3,24 @@ local lspconfig = require("lspconfig")
 return {
 	lsp = lspconfig.clangd,
 
+	cmd = function(dispatchers, config)
+		local uv = vim.uv or vim.loop
+		local join = vim.fs.joinpath
+
+		local root = assert(config.root_dir or uv.cwd(), "no root_dir computed")
+
+		local local_bin = join(root, "bin", "clangd")
+		local mason_bin = vim.fn.stdpath("data") .. "/mason/bin/clangd"
+		local exe = (uv.fs_stat(local_bin) and local_bin) or mason_bin
+		assert(uv.fs_stat(exe), "clangd not found: " .. exe)
+
+		local argv = { exe, "--compile-commands-dir=" .. root }
+
+		local extra = { cwd = root }
+
+		return vim.lsp.rpc.start(argv, dispatchers, extra)
+	end,
+
 	options = {
 		single_file_support = true,
 	},

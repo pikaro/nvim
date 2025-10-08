9ad6ad415f8955d514afd5ca5bbf182015e1782b
David Reis <git@d-reis.com>
2025-10-08 15:15:50 +0200

    fix: Disable terraform-ls on single-file

--- lua/lsp/terraform.lua
+++ lua/lsp/terraform.lua
@@ -3,6 +3,9 @@ local lspconfig = require("lspconfig")
 return {
 	lsp = lspconfig.terraformls,
 	filetypes = { "terraform", "terraform-vars", "terraform-test" },
+	options = {
+		single_file_support = false,
+	},
 	on_attach = function(client, bufnr)
 		require("treesitter-terraform-doc").setup({
 			command_name = "OpenDoc",

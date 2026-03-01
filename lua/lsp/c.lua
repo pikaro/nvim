3dedbdc1a8f2d3c02f5297f5a967ff024ece9281
David Reis <git@d-reis.com>
2026-03-01 14:48:58 +0100

    fix: clangd

--- lua/lsp/c.lua
+++ lua/lsp/c.lua
@@ -1,5 +1,14 @@
 local lspconfig = require("lspconfig")
 
+local function find_root(fname)
+	local cwd = nil
+	if not fname or fname == "" then
+		cwd = vim.uv.cwd()
+	end
+	return lspconfig.util.root_pattern(".clangd", "compile_commands.json", "compile_flags.txt", ".git")(fname or cwd)
+		or lspconfig.util.path.dirname(fname or cwd)
+end
+
 return {
 	lsp = "clangd",
 
@@ -14,11 +23,9 @@ return {
 		local exe = (uv.fs_stat(local_bin) and local_bin) or mason_bin
 		assert(uv.fs_stat(exe), "clangd not found: " .. exe)
 
-		local argv = { exe, "--compile-commands-dir=" .. root }
-
 		local extra = { cwd = root }
 
-		return vim.lsp.rpc.start(argv, dispatchers, extra)
+		return vim.lsp.rpc.start({ exe }, dispatchers, extra)
 	end,
 
 	options = {
@@ -31,8 +38,21 @@ return {
 
 	filetypes = { "c", "cpp", "arduino" },
 
-	root_dir = function(fname)
-		return lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname)
-			or lspconfig.util.path.dirname(fname)
+	root_dir = function(bufnr, on_dir)
+		local uv = vim.uv
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+
+		local root = find_root(fname)
+
+		if fname:find(vim.fn.expand("~/.platformio/packages/"), 1, true) then
+			for _, client in ipairs(vim.lsp.get_clients({ name = "clangd" })) do
+				if client.config and client.config.root_dir then
+					root = client.config.root_dir
+					break
+				end
+			end
+		end
+
+		on_dir(root or uv.cwd())
 	end,
 }
